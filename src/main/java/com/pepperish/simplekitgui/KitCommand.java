6600bd7b02bfeceee7ee074193b4c07c34c2b9e5
amatthewg <aidengrimsey143@gmail.com>
2023-10-18 16:14:52 -0400

    Initial

--- src/main/java/com/pepperish/simplekitgui/KitCommand.java
+++ src/main/java/com/pepperish/simplekitgui/KitCommand.java
@@ -1,2 +1,17 @@
-package com.pepperish.simplekitgui;public class KitCommand {
+package com.pepperish.simplekitgui;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class KitCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        String message = "&cThis is a test message that should appear red, &3and this should appear green.";
+        Player p = (Player) commandSender;
+        p.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+        return true;
+    }
 }
